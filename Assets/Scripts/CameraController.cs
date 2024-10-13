101e348edbb9eb60af6fb6d71c8fac567395aa65
Ga1kinSasha <115646309+Ga1kinSasha@users.noreply.github.com>
2024-10-13 15:23:08 +0500

    start
    baza

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform target;
+    [SerializeField] private float smoothSpeed;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        //transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x, target.position.y, transform.position.z), smoothSpeed * Time.deltaTime);
+    }
+}
