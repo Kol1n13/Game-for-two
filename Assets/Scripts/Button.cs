890776b557c2e927ed91807da8b533b6b6614fc4
Kol1n13 <kolin.1312@yandex.ru>
2024-10-27 17:38:55 +0500

    Floor-Button

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,7 +1,62 @@
-using UnityEngine;
+using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+using UnityEngine.Events; 
 
-public class Button : UnityEngine.UIElements.Button
-{
-    public bool IsActive;
-    
+public class PressurePad: MonoBehaviour {
+ 
+    public UnityEvent OnActivate; 
+    public UnityEvent OnDeactivate;
+    public GameObject door;
+
+    public float deactivationDelay = 6f;
+
+    int objectsInContact;
+    Coroutine waitingToDeactivate;
+
+
+    IEnumerator Timer() { 
+        yield return new WaitForSeconds(deactivationDelay);
+
+        // Tell our listeners we're switched OFF.
+        if (OnDeactivate != null) 
+            OnDeactivate.Invoke(); 
+
+        // Return to ready-to-activate state:
+        waitingToDeactivate = null;
+    } 
+
+    void OnTriggerEnter2D(Collider2D other) { 
+        Debug.Log("Hello");
+        var a = door.GetComponent<Animation>();
+        var coll = door.GetComponent<BoxCollider2D>();
+        coll.enabled = false;
+        Debug.Log(a);
+        a.Play();
+        objectsInContact++; 
+        
+        // Pressure pad pressed.
+        if (objectsInContact == 1) {
+
+            if (waitingToDeactivate != null) {
+                // Cancel deactivation timer. 
+                StopCoroutine(waitingToDeactivate);
+                waitingToDeactivate = null;
+
+            } else if (OnActivate != null) {
+                // Tell our listeners we're switched ON.
+                OnActivate.Invoke(); 
+            } 
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) { 
+        objectsInContact--; 
+
+        // Pressure pad released.
+        if (objectsInContact == 0) {
+            // Start a delay before telling anyone.
+            waitingToDeactivate = StartCoroutine(Timer()); 
+        } 
+    }
 }
