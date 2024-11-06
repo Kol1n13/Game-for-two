1740d59a8c18c26aafa6b13e217a1ff2591cacbf
Ga1kinSasha <115646309+Ga1kinSasha@users.noreply.github.com>
2024-11-07 02:43:23 +0500

    Молик

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,77 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    private int currentHealth;
+    private bool isTouchingToxicMolly = false;
+    public int damagePerTick = 1;
+    public float damageInterval = 0.1f;
+    private float damageTimer;
+
+    public Slider healthSlider; 
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = currentHealth; 
+        healthSlider.gameObject.SetActive(false); 
+    }
+
+    private void Update()
+    {
+        if (isTouchingToxicMolly)
+        {
+            if (!healthSlider.gameObject.activeSelf)
+            {
+                healthSlider.gameObject.SetActive(true);
+            }
+
+            damageTimer += Time.deltaTime;
+            if (damageTimer >= damageInterval)
+            {
+                currentHealth -= damagePerTick;
+                currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth); 
+                healthSlider.value = currentHealth; 
+                damageTimer = 0f;
+                if (currentHealth <= 0)
+                {
+                    RestartLevel(); 
+                }
+            }
+        }
+        else
+        {
+            if (healthSlider.gameObject.activeSelf)
+            {
+                healthSlider.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("ToxicMolly"))
+        {
+            isTouchingToxicMolly = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("ToxicMolly"))
+        {
+            isTouchingToxicMolly = false;
+        }
+    }
+
+    private void RestartLevel()
+    {
+        // Перезапускаем текущую сцену
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
